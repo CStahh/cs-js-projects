1d00919de49e2ec12ee083621b5b865cb4661390
Chris <chris.schwint@gmail.com>
2022-06-03 19:45:51 -0400

    info on var types

--- index.js
+++ index.js
@@ -3,3 +3,30 @@ var firstName = 'Chris';
 console.log(firstName);
 firstName = 'Phil';
 console.log(firstName);
+
+// DATA TYPES
+// string
+var person = 'Christopher';
+
+// integer
+var num1 = 5;
+var num2 = 10;
+var add = num1 * num2;
+
+// bool
+var falisity = true;
+
+//object
+var dude = {
+    givenName: 'John',
+    lastName: 'Smith',
+    age: 50,
+};
+
+//undefined
+var mystery;
+
+//null
+var zero = null;
+
+

fdfe2be53f4dfe2e970eafee600b5b2c653ed905
Chris <chris.schwint@gmail.com>
2022-06-05 19:19:20 -0400

    learning basic js math

--- index.js
+++ index.js
@@ -29,4 +29,39 @@ var mystery;
 //null
 var zero = null;
 
+//Maths
+var a = 5;
+var b = 7;
+var result = a + b;
+console.log(result);
+
+//var min = Math.min(3, 12, 2);
+//var max = Math.max(3, 12, 2);
+console.log('min:', Math.min(3, 12, 2));
+console.log('max:', Math.max(3, 12, 2));
+
+console.log('ceil', Math.ceil(2.2));
+console.log('ceil', Math.ceil(2));
+console.log('ceil', Math.ceil(3.9));
+
+console.log('floor', Math.floor(9.8));
+console.log('floor', Math.floor(9));
+console.log('floor', Math.floor(3.8647382));
+
+// 0.9, 4.8, 1.5
+var small = Math.min(0.9, 4.8, 1.5);
+
+console.log('smallest/down', Math.floor(small));
+console.log('smallest/down', Math.floor(Math.min(0.9, 4.8, 1.5)));
+console.log('largest/up:', Math.ceil(Math.max(0.9, 4.8, 1.5)));
+
+console.log('random whole', Math.floor(Math.random() * 10));
+
+
+
+
+
+
+
+
 
